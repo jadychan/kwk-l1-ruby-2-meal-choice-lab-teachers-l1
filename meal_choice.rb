a6a39a51b11633a7721873160e0939f5f72a00ee
jadychan <jadychan123@gmail.com>
2018-06-05 05:01:12 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -12,7 +12,7 @@ puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
 
 # call your methods here
-
+puts snacks("Chipotle")
 
 # Call your methods without any arguments here
 puts snacks
